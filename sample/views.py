ffb4e85191c4254395efed4c6d45ad4a304d9208
Anar Rahimli <rahanar@gmail.com>
2013-10-23 12:16:47 -0400

    commented the code

--- sample/views.py
+++ sample/views.py
@@ -23,44 +23,52 @@ def process_login(request):
     password does not need to be encrypted by javascript)
     """
 
+    # Get username and password
     try:
         username = request.POST['username']
         password = request.POST['password']
     except KeyError:
         return HttpResponseBadRequest()
 
-    signin_page = reverse_lazy("home")
-    doctor_page = reverse_lazy("doctor")
-    worker_page = reverse_lazy("field")
+    # redirections
+    signin_page = reverse_lazy("home")  # redirect to login page if failed
+    doctor_page = reverse_lazy("doctor")  # redirect to doctor's page
+    worker_page = reverse_lazy("field")  # redirect to worker's page
 
+    # Check if username is db
     try:
         User.objects.get(username=username)
     except ObjectDoesNotExist:
         return HttpResponseRedirect("%s?e=u" % signin_page)
 
+    # authenticate the user and then log the user in
     user = authenticate(username=username, password=password)
     if user is not None:
         if user.is_active:
             try:
-                Doctor.objects.get(user=user)
+                Doctor.objects.get(user=user)  # check if doctor
                 login(request, user)
                 return HttpResponseRedirect(request.POST.get('next',
                                                              doctor_page))
-            except ObjectDoesNotExist:
+            except ObjectDoesNotExist:  # else the worker
                 try:
                     Worker.objects.get(user=user)
                     login(request, user)
                     return HttpResponseRedirect(request.POST.get('next',
                                                                  worker_page))
-                except ObjectDoesNotExist:
+                except ObjectDoesNotExist:  # if niether worker or doctor
+                    # must be admin, but we don't allow admins login from here
                     return HttpResponseRedirect("%s?e=a" % signin_page)
         else:
+            # redirect if account is not active
             return HttpResponseRedirect("%s?e=a" % signin_page)
     else:
+        # redirect if wrong password
         return HttpResponseRedirect("%s?e=p" % signin_page)
 
 
 def display_doctor(request):
+    '''Load doctor information to doctor's display page'''
     doctor = request.user.doctor
 
     return render_to_response('doctor.html', {
@@ -72,6 +80,7 @@ def display_doctor(request):
 
 
 def display_field_worker(request):
+    '''Load worker information to worker's display page'''
     worker = request.user.worker
 
     return render_to_response('fieldworker.html', {
